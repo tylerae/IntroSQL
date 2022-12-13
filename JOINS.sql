664cd4efa88a823d5e68fd0c9b87dbb004a82683
tylerae <84347788+tylerae@users.noreply.github.com>
2022-12-12 20:01:07 -0700

    Update JOINS.sql

--- JOINS.sql
+++ JOINS.sql
@@ -25,7 +25,33 @@ FROM employees
 WHERE first_name = 'Berni'
 ORDER BY salary asc;
 
+-- A function that will update the subscriber info to show mare info about dates
+UPDATE subscriber
+SET 
+	subscribtionStartDate = subscriberKey,
+    subscriptionLength = (DATENOW() - subscriptionStartDate),
+    subscriptionEnd = (subscriptionKey + subscriptionLength);
+    
+-- Preforming math on data in SQL 
+update product
+set price = listprice / 3
+where list_price = (Round(price , 2));
 
+-- The following commands are used to delete data // use very carefully 
+ALTER TABLE product
+DROP COLUMN list_price;
+
+-- Creating a table 
+CREATE TABLE IF NOT EXISTS `mydb`.`Student` (
+  `student_id` INT NOT NULL,
+  `fname` VARCHAR(45) NOT NULL,
+  `lname` VARCHAR(45) NULL,
+  `gender` VARCHAR(45) NOT NULL,
+  `city` VARCHAR(45) NOT NULL,
+  `state` VARCHAR(45) NOT NULL,
+  `dob` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`student_id`))
+ENGINE = InnoDB;
 
 
 
