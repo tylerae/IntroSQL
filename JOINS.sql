48b2c292b31917a7d1500bf47349393a662812f7
tylerae <84347788+tylerae@users.noreply.github.com>
2022-12-12 19:53:37 -0700

    Update JOINS.sql

--- JOINS.sql
+++ JOINS.sql
@@ -1,35 +1,25 @@
--- Question 1 
-SELECT artfile
-FROM artwork
-WHERE arttype = 'impressionism';
-
--- Question 2
+-- Simple select to filter data with condition
 SELECT artfile
 FROM artwork
 WHERE artdescription in ('flowers' , 'flower');
 
--- Question 3 
+-- Diving into join statements combine data with an inner join
 SELECT fname , lname , artist.title
 FROM artist 
 INNER JOIN artwork ON artist.title = artwrok.title;
 
--- Question 4 
+-- A left join with a final filter to organize by magazine name in decending order
 SELECT magazine.magazineName , magazine.subscriberLastName , magazine.subscriberFirstName
 FROM magazine 
 LEFT JOIN subscriber ON magzine.subscriberLastName = subscriber.subscriberLastName , magazine.subscriberFirstName = subscriber.subscriberFirstName;
 ORDER BY magazineName decs;
 
--- Question 5
-SELECT magazine.magazineName
-FROM magazine
-RIGHT JOIN subscriber ON magazine.magazineName = subscriber.subscriptions;
-
--- Question 6
+-- Joining tables with an AND statment
 SELECT employees.last_name , employeesfirst_name
 FROM employees
 LEFT JOIN dept_emp.emp_no = employees.last_name AND employees.first_name;
 
--- Question 7
+-- A quik filter that finds very specific data
 SELECT first_name 
 FROM employees 
 WHERE first_name = 'Berni'
